279a0275b7d064521ea01ec25732a96ead76c239
indra <indrasetianugroho266@gmail.com>
2021-01-05 14:28:01 +0700

    api history travel order

--- travel-versi2/controllers/__init__.py
+++ travel-versi2/controllers/__init__.py
@@ -2,3 +2,4 @@ from . import order
 from . import pool
 from . import schedule
 from . import report
+from . import api
---
+++ travel-versi2/controllers/api.py
@@ -0,0 +1,24 @@
+from odoo import http
+from odoo.http import request
+
+
+class TesApi(http.Controller):
+
+    @http.route('/api/history_travel/', type='json', auth='user')
+    def history_travel(self, **rec):
+        id = int(rec['partner_id'])
+        travels = request.env['travel.order'].search([('partner_id', '=', id)])
+        travel = []
+        for i in travels:
+            val = {
+                'name': i.name,
+                'departure' : i.departure.name,
+                'destination' : i.destination.name,
+                'departure_date': i.departure_date,
+                'departure_time': i.departure_time,
+                'state': i.state,
+                'schedule': i.schedule_id.vehicle.name,
+            }
+            travel.append(val)
+        data = {'status': 200, 'response': travel, 'massege': 'Success'}
+        return data
