52cdb48504f2335c8708586b4fc147792c87f9fc
anggasaputro <anggaindotraceteknologi@gmail.com>
2021-01-30 11:59:42 +0700

    Tampilan Baru

--- travel-versi2/controllers/cari_tiket.py
+++ travel-versi2/controllers/cari_tiket.py
@@ -185,3 +185,19 @@ class Caritiket(http.Controller):
     #         'pembayaran': pembayaran,
     #         'seats': seats,
     #     })
+
+    # import requests
+    #
+    # newHeaders = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+    #
+    # response = requests.post ('https://httpbin.org/post',
+    #                           data={'id': 1, 'name': 'Jessa'},
+    #                           headers=newHeaders)
+    #
+    # print ("Status code: ", response.status_code)
+    #
+    # response_Json = response.json ()
+    # print ("Printing Post JSON data")
+    # print (response_Json['data'])
+    #
+    # print ("Content-Type is ", response_Json['headers']['Content-Type'])
